8da75d569cf5efde7cfd9956d6096e90d5b158e4
edumangabeira <edu.mangaba@gmail.com>
2019-03-29 04:09:57 -0300

    fix corrput text and improve sample size generation

--- birds_sample.py
+++ birds_sample.py
@@ -1,32 +1,72 @@
+# -*-encoding:utf-8-*-
 from openpyxl import Workbook
+# from openpyxl import load_workbook
 import random as rd
+from string import ascii_uppercase
+
+'''
+Generates random values for lables of a pseudo-sample. For this example, I'm using
+the number and types of spotted birds while going for a walk in the park as the sample.
+The bird names/types will compose the labels(species).
+It will also write the labels and frequencies on a spreadsheet(xlsx).
+'''
 
 
 class BirdSamples:
 
-    def __init__(self, species):
-        self.species = species
+    def __init__(self):
+        # self.species = species
+        self.species = []
         self.sample = {}
 
-    def generate_dataset(self):
+    def read_bird_entries(self):
+        # arq_name = input("digite o nome do arquivo com o nome das espécies:")
+        arq_name = 'birds.txt'
+        bird_list = []
+        with open(arq_name, 'r') as arq:
+            bird_list = [bird.strip() for bird in arq]
+        self.species = bird_list
+        print(bird_list)
+
+    def generate_frequency(self):
         for bird in self.species:
             self.sample[bird] = rd.randint(1, 50)
-        return
+        return self.sample
 
-    def decrp_dict(self, key, value):
-        key = []
-        value = []
+    def decrp_dict(self, dictionary):
+        species = []
+        frequency = []
 
-    def write_excel(self, rows, columns):
+        for key, value in dictionary.items():
+            species.append(key)
+            frequency.append(value)
+
+        return species, frequency
+
+    def write_excel(self, columns, rows):
+        # wb = load_workbook("birds.xlsx")
+        # ws = wb.get_sheet_by_name("Sheet1")
         wb = Workbook()
-        wb.active()
-        wb.create_sheet(title="birdsSample")
+        ws = wb.active
+        if len(columns) < 23:
+            labels = list(ascii_uppercase)
 
         for col in range(len(columns)):
-            wb.row_dimension[1] = columns
+            ws['{}{}'.format(labels[col + 1], 1)] = columns[col]
+        for row in range(len(rows)):
+            ws['{}{}'.format(labels[row + 1], 2)] = rows[row]
+        wb.save("birds.xlsx")
+
+    def generate_sample(self):
+        self.read_bird_entries()
+        sample = self.generate_frequency()
+        columns, rows = self.decrp_dict(sample)
+        self.write_excel(columns, rows)
 
 
 if __name__ == '__main__':
-    species = ["cuco", "picapau", "sabiá", "peru", "avestruz", "pombo"]
-    bird = BirdSamples(species)
-    bird.generate_dataset()
+
+    # species = ["cuco", "picapau", "sabiá", "peru", "avestruz", "pombo"]
+    # species =
+    bird = BirdSamples()
+    bird.generate_sample()
