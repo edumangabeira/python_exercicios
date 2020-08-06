70103b4486c3f7749a9f9ab2a5b991d70a792794
edumangabeira <edu.mangaba@gmail.com>
2020-08-06 15:09:00 -0300

    não lembro o que mudei :(

--- finalfantasy.py
+++ finalfantasy.py
@@ -2,10 +2,12 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
+
 url = 'https://www.gameskinny.com/1o5tb/ffxii-the-zodiac-age-hunt-club-and-trophy-rare-game-guide'
 pagina = requests.get(url)
 soup = BeautifulSoup(pagina.text, features="lxml")
 tabelas = soup.findAll('table')
+
 # locais das hunts
 locais = pd.read_html(str(tabelas[1]))
 print(locais[0])
